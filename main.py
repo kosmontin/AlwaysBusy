8647c8a98557ead72479c4a8310b8f873bc33e9c
Konstantin Beloborodov <kosta.white@gmail.com>
2022-02-22 18:15:18 +0300

    get salary info for each python developer vacancy

--- main.py
+++ main.py
@@ -2,21 +2,21 @@ import requests
 
 
 def main():
-    popular_languages = ['Python', 'Java', 'JavaScript', 'Ruby', 'PHP', 'C++', 'C#', 'C', 'Go']
     url = 'https://api.hh.ru/vacancies'
     headers = {
         'User-Agent': 'MyApp/0.1b'
     }
     params = {
+        'text': 'Программист Python',
         'area': 1,
         'period': 30
     }
-    vacancy_counters = dict()
-    for lang in popular_languages:
-        params.update({'text': f'Программист {lang}'})
-        response = requests.get(url=url, headers=headers, params=params)
-        vacancy_counters[lang] = response.json()['found']
-    print(vacancy_counters)
+    response = requests.get(url=url, headers=headers, params=params)
+    vacancies = response.json()
+    salaries = []
+    for vacancy in vacancies['items']:
+        salaries.append(vacancy['salary'])
+    print(*salaries, sep='\n')
 
 
 if __name__ == '__main__':
