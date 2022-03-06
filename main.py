064e40bd8437ce6780a0e49b64862bef98be0abb
Konstantin Beloborodov <kosta.white@gmail.com>
2022-03-06 21:04:14 +0300

    totally rewrite code

--- main.py
+++ main.py
@@ -2,54 +2,85 @@ import os
 
 import requests
 from dotenv import load_dotenv
-from pprint import pprint
+from terminaltables import AsciiTable
 from tqdm import tqdm
 
+POPULAR_LANGUAGES = ('Python', 'Java', 'JavaScript', 'Ruby', 'PHP', 'C++', 'C#', 'C', 'Go')
 
-def get_vacancy_summaries(lang):
+
+def get_hh_vacancies(lang='Python', area=1, period=30):
     url = 'https://api.hh.ru/vacancies'
     headers = {
         'User-Agent': 'MyApp/0.1b'
     }
     params = {
         'text': f'Программист {lang}',
-        'area': 1,
-        'period': 30
+        'area': area,
+        'period': period
     }
     response = requests.get(url=url, headers=headers, params=params)
     response.raise_for_status()
     vacancies_page = response.json()
-    was_found = vacancies_page['found']
-    pages_number = vacancies_page['pages']
-    has_salary = 0
-    sum_salaries = 0
-    vacancies = []
-    for page in tqdm(range(pages_number)):
+    pages = vacancies_page['pages']
+    found = vacancies_page['found']
+    vacancies = {'items': []}
+    for page in range(pages):
         for vacancy in vacancies_page['items']:
-            avg_salary = predict_rub_salary(vacancy['salary'])
-            vacancies.append({
+            vacancies['items'].append({
                 'id': vacancy['id'],
-                'average_salary': avg_salary
+                'salary': predict_rub_salary({
+                    'from': vacancy['salary']['from'],
+                    'to': vacancy['salary']['to'],
+                    'currency': vacancy['salary']['currency']
+                }) if vacancy['salary'] else None
             })
-            if avg_salary:
-                has_salary += 1
-                sum_salaries += avg_salary
         params.update({'page': page})
         response = requests.get(url=url, headers=headers, params=params)
         response.raise_for_status()
         vacancies_page = response.json()
-    summary = {
-        lang: {
-            'vacancies_found': was_found,
-            'vacancies_processed': has_salary,
-            'average_salary': int(sum_salaries / has_salary)
-        }
+    vacancies['found'] = found
+    return vacancies
+
+
+def get_sj_vacancies(lang='Python'):
+    url = 'https://api.superjob.ru/2.0/vacancies/'
+    headers = {
+        'X-Api-App-Id': os.getenv('SJ_API_KEY')
     }
-    return summary
+    params = {
+        'town': 'Москва',
+        'keyword': f'Программист {lang}',
+        'catalogues': 48
+    }
+    response = requests.get(url, headers=headers, params=params)
+    response.raise_for_status()
+    vacancies_page = response.json()
+    vacancies = {'items': []}
+    found = vacancies_page['total']
+    page = 0
+    more = True
+    while more:
+        for vacancy in vacancies_page['objects']:
+            vacancies['items'].append({
+                'id': vacancy['id'],
+                'salary': predict_rub_salary({
+                    'from': vacancy['payment_from'],
+                    'to': vacancy['payment_to'],
+                    'currency': vacancy['currency']
+                })
+            })
+        page += 1
+        more = vacancies_page['more']
+        params.update({'page': page})
+        response = requests.get(url, headers=headers, params=params)
+        response.raise_for_status()
+        vacancies_page = response.json()
+    vacancies['found'] = found
+    return vacancies
 
 
 def predict_rub_salary(salary):
-    if salary and salary['currency'] == 'RUR' and (salary['from'] or salary['to']):
+    if salary and salary['currency'] in ('RUR', 'rub') and (salary['from'] or salary['to']):
         if salary['from'] and salary['to']:
             return (int(salary['from']) + int(salary['to'])) / 2
         elif salary['from']:
@@ -58,31 +89,31 @@ def predict_rub_salary(salary):
             return int(salary['to']) * 0.8
 
 
-def get_hh_vacancies():
-    popular_languages = ['Python', 'Java', 'JavaScript', 'Ruby', 'PHP', 'C++', 'C#', 'C', 'Go']
-    langs_summary = dict()
-    for lang in popular_languages:
-        print(lang)
-        langs_summary.update(get_vacancy_summaries(lang))
-    return langs_summary
+def get_summary_by_langs(vacancies_source, langs=POPULAR_LANGUAGES):
+    summary = dict()
+    for lang in tqdm(langs, desc='Processing of popular languages: '):
+        vacancies = vacancies_source(lang)
+        salaries = []
+        for vacancy in vacancies['items']:
+            if vacancy['salary']:
+                salaries.append(vacancy['salary'])
+        summary[lang] = {
+            'vacancies_found': vacancies['found'],
+            'vacancies_processed': len(salaries),
+            'average_salary': int(sum(salaries) / len(salaries))
+        }
+    return summary
 
 
-def main():
-    url = 'https://api.superjob.ru/2.0/vacancies/'
-    headers = {
-        'X-Api-App-Id': os.getenv('SJ_API_KEY')
-    }
-    params = {
-        'town': 'Москва',
-        'keyword': 'программист',
-        'catalogues': 48
-    }
-    response = requests.get(url, headers=headers, params=params)
-    response.raise_for_status()
-    for profession in response.json()['objects']:
-        print(f"ID: {profession['id']}, {profession['profession']}, {profession['town']['title']}")
+def print_pretty_table(data, source_name=None):
+    table_data = [['Язык программирования', 'Вакансий найдено', 'Вакансий обработано', 'Средняя зарплата']]
+    for lang, values in data.items():
+        table_data.append([lang, values['vacancies_found'], values['vacancies_processed'], values['average_salary']])
+    table = AsciiTable(table_data, source_name)
+    print('\n', table.table)
 
 
 if __name__ == '__main__':
     load_dotenv()
-    main()
+    print_pretty_table(get_summary_by_langs(get_sj_vacancies), 'SuperJob Moscow')
+    print_pretty_table(get_summary_by_langs(get_hh_vacancies), 'HeadHunter Moscow')
