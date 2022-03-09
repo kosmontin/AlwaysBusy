c357e99edd018b83f2301ccc33d1d997f27c9494
Konstantin Beloborodov <kosta.white@gmail.com>
2022-03-09 14:43:36 +0300

    extract params in function

--- main.py
+++ main.py
@@ -28,11 +28,10 @@ def get_hh_vacancies(lang='Python', area=1, period=30):
         for vacancy in vacancies_page['items']:
             vacancies['items'].append({
                 'id': vacancy['id'],
-                'salary': predict_rub_salary({
-                    'from': vacancy['salary']['from'],
-                    'to': vacancy['salary']['to'],
-                    'currency': vacancy['salary']['currency']
-                }) if vacancy['salary'] else None
+                'salary': predict_rub_salary(
+                    vacancy['salary']['from'],
+                    vacancy['salary']['to'],
+                    vacancy['salary']['currency']) if vacancy['salary'] else None
             })
         params.update({'page': page})
         response = requests.get(url=url, headers=headers, params=params)
@@ -63,11 +62,7 @@ def get_sj_vacancies(lang='Python'):
         for vacancy in vacancies_page['objects']:
             vacancies['items'].append({
                 'id': vacancy['id'],
-                'salary': predict_rub_salary({
-                    'from': vacancy['payment_from'],
-                    'to': vacancy['payment_to'],
-                    'currency': vacancy['currency']
-                })
+                'salary': predict_rub_salary(vacancy['payment_from'], vacancy['payment_to'], vacancy['currency'])
             })
         page += 1
         more = vacancies_page['more']
@@ -79,14 +74,14 @@ def get_sj_vacancies(lang='Python'):
     return vacancies
 
 
-def predict_rub_salary(salary):
-    if salary and salary['currency'] in ('RUR', 'rub') and (salary['from'] or salary['to']):
-        if salary['from'] and salary['to']:
-            return (int(salary['from']) + int(salary['to'])) / 2
-        elif salary['from']:
-            return int(salary['from']) * 1.2
+def predict_rub_salary(salary_from, salary_to, currency):
+    if currency in ('RUR', 'rub') and (salary_from or salary_to):
+        if salary_from and salary_to:
+            return (int(salary_from) + int(salary_to)) / 2
+        elif salary_from:
+            return int(salary_from) * 1.2
         else:
-            return int(salary['to']) * 0.8
+            return int(salary_to) * 0.8
 
 
 def get_summary_by_langs(vacancies_source, langs=POPULAR_LANGUAGES):
